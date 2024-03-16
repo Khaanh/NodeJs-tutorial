f18014be47ec4c99879d3898f337cc0d00a95bef
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-16 14:46:21 +0100

    Module#2.7: Stream & Pipe

--- app.js
+++ app.js
@@ -12,3 +12,26 @@
 
 // // генерируем событие greet;
 // emitter.emit(eventName);
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 3003;
+
+http
+	.createServer((req, res) => {
+		res.end("Hello");
+	})
+	.listen(PORT, () => {
+		// console.log("Server running in 3003");
+	});
+
+// const writeableStream = fs.createWriteStream("hello.txt");
+// writeableStream.write("Привет мир!\n");
+// writeableStream.write("Продолжение записи\n");
+// writeableStream.end("Завершение записи\n");
+
+// const readableStream = fs.createReadStream("hello.txt");
+// readableStream.on("data", function (chunk) {
+// 	console.log("ssssssssssssssssssssssss");
+// 	console.log("chunk: ", chunk.toString());
+// });
