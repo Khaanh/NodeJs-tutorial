4dcd35e46d52467d6e19b5202e33235a33e2e7e5
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-21 19:41:55 +0100

    Module#3.1: send files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3003;
 
 // http
@@ -21,22 +22,68 @@ const PORT = 3003;
 // 		console.log(`localhost:${PORT}`);
 // 	});
 
+// http
+// 	.createServer((req, res) => {
+// 		res.setHeader("Content-Type", "text/html; charset=utf-8;");
+
+// 		if (req.url === "/") {
+// 			res.statusCode = 302; // временная переадресация
+// 			// на адрес localhost:3003/newpage
+
+// 			res.setHeader("Location", "/newpage");
+// 		} else if (req.url == "/newpage") {
+// 			res.write("New address");
+// 		} else {
+// 			res.statusCode = 404; // address not found
+// 			res.write("Not Found");
+// 		}
+
+// 		res.end();
+// 	})
+// 	.listen(PORT, () => {
+// 		console.log(`PORT: ${PORT}`);
+// 	});
+
+// http
+// 	.createServer((req, res) => {
+// 		console.log(`Запрошенный адрес: ${req.url}`);
+// 		// получаем путь после слеша
+
+// 		const filePath = req.url.substring(1);
+
+// 		fs.readFile(filePath, function (error, data) {
+// 			if (error) {
+// 				res.statusCode = 404;
+// 				res.end("Resourse not found");
+// 			} else {
+// 				res.end(data);
+// 				console.log(data);
+// 			}
+// 		});
+// 	})
+// 	.listen(PORT, () => {
+// 		console.log(`Server started at: ${PORT}`);
+// 	});
+
 http
 	.createServer((req, res) => {
-		res.setHeader("Content-Type", "text/html; charset=utf-8;");
+		const filePath = req.url.substring(1);
 
-		if (req.url === "/") {
-			res.statusCode = 302; // временная переадресация
-			// на адрес localhost:3003/newpage
-
-			res.setHeader("Location", "/newpage");
-		} else if (req.url == "/newpage") {
-			res.write("New address");
-		} else {
-			res.statusCode = 404; // address not found
-			res.write("Not Found");
-		}
-
-		res.end();
+		fs.readFile(filePath, (error, data) => {
+			if (error) {
+				res.statusCode = 404;
+				res.end("Not found");
+			} else {
+				const message = "Learning message";
+				const header = "header block";
+				const dataText = data
+					.toString()
+					.replace(/{header}/g, header)
+					.replace(/{message}/g, message);
+				res.end(dataText);
+			}
+		});
 	})
-	.listen(PORT);
+	.listen(PORT, () => {
+		console.log(`Server started at ${PORT}`);
+	});
