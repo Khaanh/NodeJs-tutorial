899ddd0775cf140d1ced9f89552e134ed99aa924
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-23 17:56:49 +0100

    send form

--- app.js
+++ app.js
@@ -1,89 +1,33 @@
 const http = require("http");
 const fs = require("fs");
-const PORT = 3003;
-
-// http
-// 	.createServer((req, res) => {
-// 		res.setHeader("Content-type", "text/html; charset=utf-8;");
-
-// 		if (req.url === "/home" || req.url === "/") {
-// 			res.write("<h1>Home</h1>");
-// 		} else if (req.url == "/about") {
-// 			res.write("<h1>About</h1>");
-// 		} else if (req.url == "/contact") {
-// 			res.write("<h1>Contact</h1>");
-// 		} else {
-// 			res.write("<h2>Not found</h2>");
-// 		}
-
-// 		res.end();
-// 	})
-// 	.listen(PORT, () => {
-// 		console.log(`localhost:${PORT}`);
-// 	});
-
-// http
-// 	.createServer((req, res) => {
-// 		res.setHeader("Content-Type", "text/html; charset=utf-8;");
-
-// 		if (req.url === "/") {
-// 			res.statusCode = 302; // временная переадресация
-// 			// на адрес localhost:3003/newpage
-
-// 			res.setHeader("Location", "/newpage");
-// 		} else if (req.url == "/newpage") {
-// 			res.write("New address");
-// 		} else {
-// 			res.statusCode = 404; // address not found
-// 			res.write("Not Found");
-// 		}
-
-// 		res.end();
-// 	})
-// 	.listen(PORT, () => {
-// 		console.log(`PORT: ${PORT}`);
-// 	});
-
-// http
-// 	.createServer((req, res) => {
-// 		console.log(`Запрошенный адрес: ${req.url}`);
-// 		// получаем путь после слеша
-
-// 		const filePath = req.url.substring(1);
-
-// 		fs.readFile(filePath, function (error, data) {
-// 			if (error) {
-// 				res.statusCode = 404;
-// 				res.end("Resourse not found");
-// 			} else {
-// 				res.end(data);
-// 				console.log(data);
-// 			}
-// 		});
-// 	})
-// 	.listen(PORT, () => {
-// 		console.log(`Server started at: ${PORT}`);
-// 	});
+const PORT = 3005;
 
 http
-	.createServer((req, res) => {
-		const filePath = req.url.substring(1);
+	.createServer(async (req, res) => {
+		if (req.url === "/user") {
+			let body = "";
+			console.log("body: ", body);
 
-		fs.readFile(filePath, (error, data) => {
-			if (error) {
-				res.statusCode = 404;
-				res.end("Not found");
-			} else {
-				const message = "Learning message";
-				const header = "header block";
-				const dataText = data
-					.toString()
-					.replace(/{header}/g, header)
-					.replace(/{message}/g, message);
-				res.end(dataText);
+			for await (const chunk of req) {
+				body += chunk;
+				console.log("chunk: ", chunk);
 			}
-		});
+
+			let userName = "";
+			let userAge = 0;
+
+			const params = body.split("&");
+			console.log("params: ", params);
+
+			for (param of params) {
+				const [paramName, paramAge] = param.split("=");
+				if (paramName === "userName") userName = paramName;
+				if (paramAge === "userAge") userAge = paramAge;
+				res.write(`Your name: ${paramName}. Your age: ${paramAge}`);
+			}
+			res.end();
+		} else {
+			fs.readFile("index.html", (_, data) => res.end(data));
+		}
 	})
-	.listen(PORT, () => {
-		console.log(`Server started at ${PORT}`);
-	});
+	.listen(PORT, () => console.log(`Server running on ${PORT}`));
