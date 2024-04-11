28748a3bd2c7faac35376e38dedf3acbf4cab338
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-04-11 20:17:15 +0200

    Express send res

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require("express");
-
 const app = express();
-const PORT = 3000;
 
-app.use(function (_, _, next) {
-	console.log("Middleware 1");
-	next();
+app.use(function (_, res) {
+	// res.send("<h2>Hello</h2>");
+	// res.send({ id: 6, name: "Tom" });
+	// res.send(["Tom", "Bob", "Sam"]);
+	res.send(Buffer.from("Hello Express"));
 });
 
-app.use("/about", function (_, res) {
-	console.log("Middleware 2");
-	res.send("About Middleware 2");
-});
-
-app.get("/", function (_, res) {
-	console.log("Route /");
-	res.send("Hello");
-});
-
-app.listen(PORT, console.log(`Running server ${PORT}`));
+app.listen(3000);
